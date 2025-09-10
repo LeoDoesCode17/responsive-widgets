37fd872d946e9300e4b1d0a593e259f41b322387
Leo <leonardonifinluri@gmail.com>
2025-09-10 13:09:56 +0800

    Feat: create AppRoutes class to store constant routes

---
+++ lib/routes/routes.dart
@@ -0,0 +1,14 @@
+class AppRoutes {
+  static const home = '/';
+  static const responsiveWidget = '/responsive-widget';
+  static const layoutBuilder = '/responsive-widget/layout-builder';
+  static const mediaQuery = '/responsive-widget/media-query';
+  static const expanded = '/responsive-widget/expanded';
+  static const flexible = '/responsive-widget/flexible';
+  static const aspectRatio = '/responsive-widget/aspect-ratio';
+  static const orientationBuilder = '/responsive-widget/orientation-builder';
+  static const wrap = '/responsive-widget/wrap';
+  static const inheritedWidget = '/inherited-widget';
+  static const statelessWidget = '/stateless-widget';
+  static const statefulWidget = '/stateful-widget';
+}
