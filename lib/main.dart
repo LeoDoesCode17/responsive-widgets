4152a1bb7ff1d261a9730265f66d29aea2c3ccbb
Leo <leonardonifinluri@gmail.com>
2025-09-10 13:10:48 +0800

    Feat: use AppRoutes and route generator in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_widgets/pages/entrypoint_pages.dart';
-import 'package:responsive_widgets/pages/home_page.dart';
-import 'package:responsive_widgets/pages/responsive_widget_pages.dart';
+import 'package:responsive_widgets/routes/route_generator.dart';
+import 'package:responsive_widgets/routes/routes.dart';
 
 void main() {
   runApp(const ResponsiveWidgetApp());
@@ -18,21 +17,8 @@ class ResponsiveWidgetApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomePage(),
-      routes: <String, WidgetBuilder>{
-        '/responsive-widget': (BuildContext context) => const ResponsiveEntryPoint(),
-        '/responsive-widget/layout-builder': (BuildContext context) => const LayoutBuilderPage(),
-        '/responsive-widget/media-query': (BuildContext context) => const MediaQueryPage(),
-        '/responsive-widget/expanded': (BuildContext context) => const ExpandedPage(),
-        '/responsive-widget/flexible': (BuildContext context) => const FlexiblePage(),
-        '/responsive-widget/aspect-ratio': (BuildContext context) => const AspectRatioPage(),
-        '/responsive-widget/orientation-builder': (BuildContext context) =>
-            const OrientationBuilderPage(),
-        '/responsive-widget/wrap': (BuildContext context) => const WrapPage(),
-        '/inherited-widget': (BuildContext context) => const InheritedEntryPoint(),
-        '/stateless-widget': (BuildContext context) => const StatelessEntryPoint(),
-        '/stateful-widget': (BuildContext context) => const StatefulEntryPoint(),
-      },
+      initialRoute: AppRoutes.home,
+      onGenerateRoute: RouteGenerator.generateRoute,
     );
   }
 }
